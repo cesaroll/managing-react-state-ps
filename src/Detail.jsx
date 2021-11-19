8e6f28bc8751a2580b010b2a89e71055b96c900e
Cesar Lopez <clopezlerma@wayfair.com>
2021-11-19 22:33:44 +0100

    Fetched based on a URL parameter

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
 
 const Detail = () => {
-  return <h1>Detail</h1>;
+  const {id} = useParams();
+  const {data: product, loading, error} = useFetch(`products/${id}`);
+
+  if (loading) return <Spinner/>;
+  if (error) throw error;
+
+  return (
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.descriptions}</p>
+      <p id="price">{product.price}</p>
+      <img src={`/images/${product.image}`} alt={product.category}/>
+    </div>
+  );
 }
 
 export default Detail;
