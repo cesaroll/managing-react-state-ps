c71ae239655999af0a5eebddb967d09ce0789ae2
Cesar Lopez <clopezlerma@wayfair.com>
2021-11-19 22:45:06 +0100

    Redirecting via useNavigation

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
 import PageNotFound from "./PageNotFound";
 
 const Detail = () => {
   const {id} = useParams();
+  const navigate = useNavigate();
   const {data: product, loading, error} = useFetch(`products/${id}`);
 
   if (loading) return <Spinner/>;
@@ -17,6 +18,9 @@ const Detail = () => {
       <h1>{product.name}</h1>
       <p>{product.descriptions}</p>
       <p id="price">{product.price}</p>
+      <p>
+        <button className="btn btn-primary" onClick={() => navigate("/cart")}>Add to cart</button>
+      </p>
       <img src={`/images/${product.image}`} alt={product.category}/>
     </div>
   );
