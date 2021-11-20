04d05c3146467178ea8559f3375def7dbb508132
Cesar Lopez <clopezlerma@wayfair.com>
2021-11-20 11:43:19 +0100

    Creating a Shoe Size select

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
@@ -7,17 +7,32 @@ import PageNotFound from "./PageNotFound";
 const Detail = () => {
   const {id} = useParams();
   const navigate = useNavigate();
+  const [sku, setSku] = useState("");
   const {data: product, loading, error} = useFetch(`products/${id}`);
 
   if (loading) return <Spinner/>;
   if (!product) return <PageNotFound/>;
   if (error) throw error;
 
+  const sizeArray = product.skus.map(s => s.size);
+
   return (
     <div id="detail">
       <h1>{product.name}</h1>
       <p>{product.descriptions}</p>
       <p id="price">{product.price}</p>
+
+      <select
+        id="size"
+        value={sku}
+        onChange={(e) => setSku(e.target.value)}
+      >
+        <option value="">What size?</option>
+        {product.skus.map(s => (
+          <option key={s.sku} value={s.sku}>{s.size}</option>
+        ))}
+      </select>
+
       <p>
         <button className="btn btn-primary" onClick={() => navigate("/cart")}>Add to cart</button>
       </p>
