93ff4a29aa2f6c9ba5d835754f2b8b0cf96eff64
Cesar Lopez <clopezlerma@wayfair.com>
2021-11-23 16:17:17 +0100

    Lazy initializing state and persisting state to local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -8,8 +8,18 @@ import Detail from "./Detail";
 import Cart from "./Cart";
 
 export default function App() {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem("cart")) ?? [];
+    } catch {
+      console.error("The cart could not be pared into JSON.");
+      return [];
+    }
+  });
 
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   const addToCart = (id, sku) => {
     setCart((items) => {
