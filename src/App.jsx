75f00b7d129c4481e71ccd5bbd7b1be8f777ac16
Cesar Lopez <clopezlerma@wayfair.com>
2021-11-29 21:58:56 +0100

    SAving the form submission and emptying the cart

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,11 @@ export default function App() {
         items.filter(i => i.sku !== sku) :
         items.map(i => i.sku === sku ? {...i, quantity} : i );
     });
-  }
+  };
+
+  const emptyCart = () => {
+    setCart([]);
+  };
 
   return (
     <>
@@ -55,7 +59,7 @@ export default function App() {
             <Route path="/:category" element={<Products/>}/>
             <Route path="/:category/:id" element={<Detail addToCart={addToCart}/>} />
             <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity}/>}/>
-            <Route path="/checkout" element={<Checkout cart={cart} />}/>
+            <Route path="/checkout" element={<Checkout cart={cart} emptyCart={emptyCart}/>}/>
           </Routes>
         </main>
       </div>
